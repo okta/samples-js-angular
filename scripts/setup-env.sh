7075cc4b83b5a0e9761e1fd6599797c1807314d6
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2018-01-19 15:50:01 -0800

    Updating PR with Spring sample resource server

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -5,3 +5,9 @@ sed -i -- "s/{clientId}/$CLIENT_ID/g" okta-hosted-login/src/app/.samples.config.
 
 sed -i -- "s/{yourOktaDomain}/$OKTA_DOMAIN/g" custom-login/src/app/.samples.config.ts
 sed -i -- "s/{clientId}/$CLIENT_ID/g" custom-login/src/app/.samples.config.ts
+
+# Clone the resource-server repo and update the environment
+[ ! -d samples-java-spring-mvc/ ] && git clone https://github.com/okta/samples-java-spring-mvc.git || echo "Resource server already cloned"
+cd samples-java-spring-mvc
+git checkout samples-v2-spec
+cd ../
