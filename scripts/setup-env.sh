22d2923c35a71ff8aee85740ed590769387e87a7
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2018-03-06 15:43:46 -0800

    Updating script to remove dependence on OKTA_DOMAIN env var for test

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-# The environment variables (OKTA_DOMAIN, CLIENT_ID) are set in the travis repository settings
-sed -i -- "s/{yourOktaDomain}/$OKTA_DOMAIN/g" okta-hosted-login/src/app/.samples.config.ts
+# The environment variables (ISSUER, CLIENT_ID) are set in the travis repository settings
+# Escape the backslashes in ISSUER env var
+issuer=$(echo "$ISSUER" | sed 's/\//\\\//g')
+
+sed -i -- "s/https:\/\/{yourOktaDomain}.com\/oauth2\/default/$issuer/g" okta-hosted-login/src/app/.samples.config.ts
 sed -i -- "s/{clientId}/$CLIENT_ID/g" okta-hosted-login/src/app/.samples.config.ts
 
-sed -i -- "s/{yourOktaDomain}/$OKTA_DOMAIN/g" custom-login/src/app/.samples.config.ts
+sed -i -- "s/https:\/\/{yourOktaDomain}.com\/oauth2\/default/$issuer/g" custom-login/src/app/.samples.config.ts
 sed -i -- "s/{clientId}/$CLIENT_ID/g" custom-login/src/app/.samples.config.ts
 
 # Clone the resource-server repo and update the environment
