6abfb65c599ff4c995cf73331ddd5c10253752a0
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2018-04-03 10:05:29 -0700

    Updated setup-env script to support testenv

--- scripts/setup-env.js
+++ scripts/setup-env.js
@@ -1,13 +1,16 @@
 /* eslint-disable consistent-return, no-console */
 
 const fs = require('fs');
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
 const path = require('path');
 
+// Users can also provide the testenv configuration at the root folder
+require('dotenv').config({path: path.join(__dirname, '..', 'testenv')});
+
 function updateConfig(directory) {
-  if (process.env.ISSUER === undefined || process.env.CLIENT_ID === undefined) {
-    console.log('[ERROR] Please set the ISSUER and CLIENT_ID Environment variables');
-    return;
+  if (!process.env.ISSUER || !process.env.CLIENT_ID || !process.env.USERNAME || !process.env.PASSWORD) {
+    console.log('[ERROR] Please set the necessary Environment variables (ISSUER, CLIENT_ID, USERNAME, PASSWORD)');
+    process.exit(1);
   }
 
   const file = path.join(__dirname, '..', directory, '/src/app/.samples.config.ts');
@@ -20,18 +23,14 @@ function updateConfig(directory) {
 function cloneRepository(repository, directory) {
   const dir = path.join(__dirname, '..', directory);
   if (fs.existsSync(dir)) {
-    console.log(`${directory} is already cloned.`);
+    console.log(`${directory} is already cloned. Getting latest version...`);
+    execSync(`cd ${directory} && git pull`)
     return;
   }
 
   const command = `git clone ${repository}`;
   console.log(`Cloning repository ${directory}`);
-  exec(command, (err, stdout) => {
-    if (err !== null) {
-      return console.error(err);
-    }
-    return console.log(stdout);
-  });
+  execSync(command);
 }
 
 updateConfig('custom-login');
