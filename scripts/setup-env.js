dfe612e53b72b5292d05ca11303a1c7efed4ef5e
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2018-04-03 10:21:32 -0700

    Updated CONTRIBUTING.md to add testenv support

--- scripts/setup-env.js
+++ scripts/setup-env.js
@@ -5,7 +5,7 @@ const { execSync } = require('child_process');
 const path = require('path');
 
 // Users can also provide the testenv configuration at the root folder
-require('dotenv').config({path: path.join(__dirname, '..', 'testenv')});
+require('dotenv').config({ path: path.join(__dirname, '..', 'testenv') });
 
 function updateConfig(directory) {
   if (!process.env.ISSUER || !process.env.CLIENT_ID || !process.env.USERNAME || !process.env.PASSWORD) {
