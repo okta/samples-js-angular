4358ecb0f4460fb27133354c2bfb890d2ff9fe21
Yu Ning Hee <yuninghee@gmail.com>
2018-12-02 19:35:05 -0800

    Made changes to default-config.ts to reflect what should also be on .sample.config.ts

--- okta-hosted-login/util/default-config.ts
+++ okta-hosted-login/util/default-config.ts
@@ -1,11 +1,11 @@
 export default {
   oidc: {
-    clientId: '{clientId}',
-    issuer: 'https://{yourOktaDomain}.com/oauth2/default',
-    redirectUri: 'http://localhost:8080/implicit/callback',
-    scope: 'openid profile email',
+    clientId: "0oahyygobyKQWaomA0h7",
+    issuer: "https://dev-838726.oktapreview.com",
+    redirectUri: "https://localhost:4200/implicit/callback",
+    scope: "openid profile email"
   },
   resourceServer: {
-    messagesUrl: 'http://localhost:8000/api/messages',
-  },
+    messagesUrl: "http://localhost:8000/api/messages"
+  }
 };
