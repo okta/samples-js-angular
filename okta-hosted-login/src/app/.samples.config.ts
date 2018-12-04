7090c5acf3a34554e1a7c59ff3c9d3ad1442c196
Robert Campbell <robertgcampbell95@gmail.com>
2018-12-03 17:33:48 -0800

    Update okta authentication

--- okta-hosted-login/src/app/.samples.config.ts
+++ okta-hosted-login/src/app/.samples.config.ts
@@ -1,6 +1,6 @@
 export default {
     oidc: {
-      clientId: '0oai15h1i0pZDdpBm0h7',
+      clientId: '0oai15h1i0pZDdpBm0h7s',
       issuer: 'https://dev-838726.oktapreview.com/',
       redirectUri: 'http://104.248.178.198:4200/implicit/callback',
       scope: 'openid profile email',
