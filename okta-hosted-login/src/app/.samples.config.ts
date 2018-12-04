4965648fac35cbf8ca344eb82723d8830d0a89e3
Vivian Leung <vivz753@gmail.com>
2018-12-04 01:09:55 -0800

    local credentials

--- okta-hosted-login/src/app/.samples.config.ts
+++ okta-hosted-login/src/app/.samples.config.ts
@@ -1,12 +1,11 @@
 export default {
-    oidc: {
-      clientId: '0oai1gohhtcxbBjLv0h7',
-      issuer: 'https://dev-838726.oktapreview.com/',
-      redirectUri: 'http://104.248.178.198:4200/implicit/callback',
-      scope: 'openid profile email',
-    },
-    resoureServer: {
-        messagesUrl: 'http://localhost:8000/api/messages',
-    },
-
+  oidc: {
+    clientId: '0oai1gmnyxbdd1FJO0h7',
+    issuer: 'https://dev-323697.oktapreview.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/implicit/callback',
+    scope: 'openid profile email',
+  },
+  resourceServer: {
+    messagesUrl: 'http://localhost:8000/api/messages',
+  },
 };
