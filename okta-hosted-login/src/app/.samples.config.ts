f0f3a30ba5a52b5299d2c84fb59738d90a318dbb
Yu Ning Hee <yuninghee@gmail.com>
2018-12-05 16:08:17 -0800

    https for droplet app

--- okta-hosted-login/src/app/.samples.config.ts
+++ okta-hosted-login/src/app/.samples.config.ts
@@ -1,11 +1,11 @@
 export default {
   oidc: {
-    clientId: '0oai1gohhtcxbBjLv0h7',
-    issuer: 'https://dev-323697.oktapreview.com/oauth2/default',
-    redirectUri: 'http://104.248.178.198:4200/implicit/callback',
-    scope: 'openid profile email',
+    clientId: "0oai1gohhtcxbBjLv0h7",
+    issuer: "https://dev-323697.oktapreview.com/oauth2/default",
+    redirectUri: "https://104.248.178.198:4200/implicit/callback",
+    scope: "openid profile email"
   },
   resourceServer: {
-    messagesUrl: 'http://localhost:8000/api/messages',
-  },
+    messagesUrl: "http://localhost:8000/api/messages"
+  }
 };
