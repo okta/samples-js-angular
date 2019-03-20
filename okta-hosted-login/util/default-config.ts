27f7a1c644a8cf333787ebb3a4cbf178a7684ae3
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2019-03-20 11:43:28 -0700

    Adds support to disable HTTPS check for testing

--- okta-hosted-login/util/default-config.ts
+++ okta-hosted-login/util/default-config.ts
@@ -4,6 +4,9 @@ export default {
     issuer: 'https://{yourOktaDomain}.com/oauth2/default',
     redirectUri: 'http://localhost:8080/implicit/callback',
     scope: 'openid profile email',
+    testing: {
+      disableHttpsCheck: false
+    }
   },
   resourceServer: {
     messagesUrl: 'http://localhost:8000/api/messages',
