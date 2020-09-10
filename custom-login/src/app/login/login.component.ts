0d0adf8f27419ff1334cdf0f54831c3e0568e9ea
Matt Raible <matt.raible@okta.com>
2020-09-10 17:32:41 -0600

    Fix onAuthRequired arguments and OIDC spelling

--- custom-login/src/app/login/login.component.ts
+++ custom-login/src/app/login/login.component.ts
@@ -25,7 +25,7 @@ export class LoginComponent implements OnInit {
   constructor() {
     this.signIn = new OktaSignIn({
       /**
-       * Note: when using the Sign-In Widget for an ODIC flow, it still
+       * Note: when using the Sign-In Widget for an OIDC flow, it still
        * needs to be configured with the base URL for your Okta Org. Here
        * we derive it from the given issuer for convenience.
        */
