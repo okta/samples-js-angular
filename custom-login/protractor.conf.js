4e16381841f6445de684bcdd2743bf5c3d1cd9c9
Aaron Granick <aaron.granick@okta.com>
2020-12-08 23:08:40 +0000

    add 8ac0n scripts
    OKTA-347959
    <<<Jenkins Check-In of Tested SHA: 8a163233d4dfefd265740211cf3f96c3c5707529 for eng_productivity_ci_bot_okta@okta.com>>>
    Artifact: samples-js-angular

--- custom-login/protractor.conf.js
+++ custom-login/protractor.conf.js
@@ -9,32 +9,23 @@
  *
  * See the License for the specific language governing permissions and limitations under the License.
  */
-
+/* eslint-disable */
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
+// Load environment variables
+require('./env');
 
-exports.config = {
-  allScriptsTimeout: 11000,
-  specs: [
-    './e2e/**/*.e2e-spec.ts'
-  ],
-  capabilities: {
-    'browserName': 'chrome'
-  },
-  directConnect: true,
-  baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
-  },
-  onPrepare() {
-    require('ts-node').register({
-      project: 'e2e/tsconfig.e2e.json'
-    });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+var E2E_DIR = '../okta-oidc-tck/e2e-tests/custom-login/';
+var config = require(E2E_DIR + 'conf.js').config;
+config.specs = config.specs.map(function (path) {
+  return E2E_DIR + path;
+});
+
+['CLIENT_ID', 'ISSUER', 'USERNAME', 'PASSWORD'].forEach(function(key) {
+  if (!process.env[key]) {
+    throw new Error('Environment variable "' + key + '" is not set');
   }
-};
+});
+
+exports.config = config;
