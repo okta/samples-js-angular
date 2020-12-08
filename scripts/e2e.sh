8a163233d4dfefd265740211cf3f96c3c5707529
Aaron Granick <aaron.granick@okta.com>
2020-12-08 11:59:51 -0800

    results file dir

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -12,7 +12,7 @@ export TRAVIS=true
 export DBUS_SESSION_BUS_ADDRESS=/dev/null
 
 export TEST_SUITE_TYPE="junit"
-export TEST_RESULT_FILE_DIR="${REPO}/build2/e2e"
+export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/junit"
 
 export ISSUER=https://samples-javascript.okta.com/oauth2/default
 export CLIENT_ID=0oapmwm72082GXal14x6
