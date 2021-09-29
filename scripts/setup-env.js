363fcf299feddd622878601320974664dc4b32f5
Shuo Wu <wushuo2010@gmail.com>
2021-09-28 21:56:36 -0400

    use tck fix branch

--- scripts/setup-env.js
+++ scripts/setup-env.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const { execSync } = require('child_process');
 const path = require('path');
 
-function cloneRepository(repository, directory) {
+function cloneRepository(repository, directory, branch = 'master') {
   const dir = path.join(__dirname, '..', directory);
   if (fs.existsSync(dir)) {
     console.log(`${directory} is already cloned. Getting latest version...`);
@@ -12,11 +12,11 @@ function cloneRepository(repository, directory) {
     return;
   }
 
-  const command = `git clone ${repository}`;
+  const command = `git clone --single-branch --branch ${branch} ${repository}`;
   console.log(`Cloning repository ${directory}`);
   execSync(command);
 }
 
 cloneRepository('https://github.com/okta/samples-nodejs-express-4.git', 'samples-nodejs-express-4');
 execSync(`cd ${path.join(__dirname, '..', 'samples-nodejs-express-4')} && npm install --unsafe-perm`);
-cloneRepository('https://github.com/okta/okta-oidc-tck.git', 'okta-oidc-tck');
+cloneRepository('https://github.com/okta/okta-oidc-tck.git', 'okta-oidc-tck', 'sw-fix-oie-specs');
