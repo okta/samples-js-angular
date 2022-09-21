ad9f657d70bac94eeadab4f4015eda78c41e5e78
denys.oblohin <denys.oblohin@okta.com>
2022-09-21 17:19:40 +0300

    TS fix for new SIW

--- custom-login/src/app/login/login.component.ts
+++ custom-login/src/app/login/login.component.ts
@@ -41,7 +41,7 @@ export class LoginComponent implements OnInit {
           'primaryauth.title': 'Sign in to Angular & Company',
         },
       },
-      authClient: oktaAuth,
+      authClient: oktaAuth as any,
       useInteractionCodeFlow: sampleConfig.widget.useInteractionCodeFlow === 'true',
     });
   }
