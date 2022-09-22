2e5902753cba79ed37ec16fac27eadb2334d405f
denys.oblohin <denys.oblohin@okta.com>
2022-09-22 20:42:01 +0300

    WAIT_FOR_MESSAGE

--- env.js
+++ env.js
@@ -12,3 +12,4 @@ if (fs.existsSync(TESTENV)) {
 }
 process.env.CLIENT_ID = process.env.CLIENT_ID || process.env.SPA_CLIENT_ID;
 process.env.USE_INTERACTION_CODE = process.env.USE_INTERACTION_CODE || false;
+process.env.WAIT_FOR_MESSAGE = 'Compiled successfully';
