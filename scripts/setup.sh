6aba13d4804b7d433b8325eb6da15c6225b6fb82
Jared Perreault <90656038+jaredperreault-okta@users.noreply.github.com>
2026-06-17 14:00:35 -0400

    snyk fix (#187)
    OKTA-1095067 snyk fix

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,9 +4,7 @@
 export NVM_DIR="/root/.nvm"
 setup_service node v18.19.1 &> /dev/null
 
-# Revert the cache-min setting, since the internal cache does not apply to
-# these repos (and causes problems in lookups)
-npm config set cache-min 10
+npm config set registry https://registry.npmjs.org/
 
 # Install dependences.
 if ! npm install --ignore-scripts; then
